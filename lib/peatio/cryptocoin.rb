55112101f7c4736f0e4e370bb86b7ef26253b9af
Gonzo <72076492+GonzoTheDev@users.noreply.github.com>
2021-03-16 13:44:40 +0000

    Update and rename monero.rb to cryptocoin.rb

---
+++ lib/peatio/cryptocoin.rb
@@ -0,0 +1,10 @@
+require 'peatio/services/blockchain_service/cryptocoin'
+require 'peatio/services/wallet_service/cryptocoin-wallet-rpc'
+require 'peatio/client/blockchain_client/cryptocoin'
+require 'peatio/client/wallet_client/cryptocoind'
+
+module Peatio
+  module CryptoCoin
+    require "peatio/cryptocoin/version"
+  end
+end
