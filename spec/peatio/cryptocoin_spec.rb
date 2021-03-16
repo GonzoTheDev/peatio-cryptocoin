5416d5f37f7435e5c997ac537524b3ccec40eac3
Gonzo <72076492+GonzoTheDev@users.noreply.github.com>
2021-03-16 13:43:02 +0000

    Update and rename monero_spec.rb to cryptocoin_spec.rb

--- spec/peatio/cryptocoin_spec.rb
+++ spec/peatio/cryptocoin_spec.rb
@@ -1,6 +1,6 @@
-RSpec.describe Peatio::Monero do
+RSpec.describe Peatio::CryptoCoin do
   it "has a version number" do
-    expect(Peatio::Monero::VERSION).not_to be nil
+    expect(Peatio::CryptoCoin::VERSION).not_to be nil
   end
 
   it "does something useful" do
